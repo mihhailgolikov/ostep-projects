aa0e222d76beac003600d237a443f6ac77c61591
mihhailgolikov <misha1998@gmail.com>
2020-01-22 16:29:52 -0500

    Add files via upload
    Re-implemented the file reading code, now taking in multiple files from the shell parameters, rather than using scan.
    Implemented writing the file out line by line, making sure there are no new line chars.
    Implemented an error message that occurs during the program when it is unable to open a file.
    Implemented exit codes for successful/unsuccessful cases.

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,61 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define FILESIZE 50
-#define NC 0     //No Comment State
-#define C  1   //Comment State
-#define PROC 2   //Pre Open Comment State
-#define PTOC 3  //Post Open Comment State
-#define ST 4    //String State
-#define C1 5    //Second Comment state
-
-
-int main()
-
+int main(int argc, char *argv[])
 {
+	int i; // incrementer/counting variable for the amount of files to be read/amnt. of arguments 
+	FILE *fp; // declaration of file pointer in order to be able to open file
+	char c; // the current character thats being looked at/written  (originally int c).
 
-    FILE *fp;
-    char output[FILESIZE];
-    char clearOutput[FILESIZE];
-    char filename[100];
-    char  cc;                                  //Current character
-    int lc =1;                                 //Line Count
-                              //Starting state = No comment state
-    char temp = '\n';
-    int count = 0;
-    int endCount = 0;
-    int endloop = 0;
+	for (i = 1; i < argc; i++) // for all of the arguments within the command prompt
+	{
+		fp = fopen(argv[i], "r"); // open each file that is declared by the user in the command line prompt, and make the file pointer point to it
+		if (fp == NULL) // if the file pointer is null, then
+		{
+			fprintf(stderr, "The function 'wcat' can't open %s\n", argv[i]); //formatted as a compiler error, letting the user know which file cannot be opened
+			continue; // using continue to exit the loop when the condition is returned as false, as not all of the files have to return an error
+		}
 
-	printf("Enter file name to read: ");
-	scanf("%s", filename);
-    fp = fopen(filename, "r");
+		while ((c = getc(fp)) != EOF) // while the current character is NOT the end of the file
+			putchar(c); // write the contents that are being read as an output in the console
 
-                     //Open File
-    if (fp == NULL){
-        printf("Could not open file %s",filename);
-        return 1;
-    }
+		fclose(fp); // make sure to close the file just to be safe, even though it is closed when wcat.c ends anyway
+	}
 
-    cc= fgetc(fp);
-
-    while (cc!= EOF ){
-
-        if(cc == '\n'){
-			lc++;
-        }
-        cc= getc(fp);
-      }
-
-		if(cc == EOF){
-			for(int i = 0; i<FILESIZE; i++){
-				output[i]= NULL;
-        }
-
-    }
-    output[count]= '\0';
-    printf("%s",output);
-
-    fclose(fp);
-
-    return 0;
 }
