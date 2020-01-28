81fd53a2d72cd5fe1740b6fe656943e8d7724211
Mihhail Golikov - tug42787 <misha1998@gmail.com>
2020-01-28 01:47:44 -0500

    Add files via upload
    Attempting to implement the compression algorithm

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE (1024*1024)
+
 
 int main(int argc, char *argv[]) {
 
-    int c[4096]; 
+    char c[4096]; 
 	int	c2[4096];
-    FILE *fp = fopen(argv[1], "r");
+    FILE *fp = fopen(argv[1], "rb");
+
+	int rep;
 
 
 
     while (!feof(fp)) {
-        size_t bytes = fread(c, 4, sizeof(c), fp);
+        size_t bytes = fread(c, 1, sizeof(c), fp);
         if (bytes) {
-			while(fread(c,4,sizeof(c), fp) == fread(c+1,4,sizeof(c+1), fp)) { // while the current character is the same as the next character
-				fwrite(c, 4, bytes, stdout);
-			}	
+			fwrite(c, 1, bytes, stdout); 
+			printf("\n");
+			
+			if(fread(c,1,sizeof(c), fp) == fread(c+1,1,sizeof(c+1), fp)) { // while the current character is the same as the next character
+				printf("if statement");
+				rep++;
+				if (fread(c,4,sizeof(c), fp) != fread(c+1,4,sizeof(c+1), fp)) { // used when the next char is different than current char, to break out of loop
+					// fwrite the int and the char
+					printf("rep number %d of char", &rep);
+					rep = 0;
+					break; // break out of it
+				}
+				break;
+			}
+			else
+				fwrite(c, 1, bytes, stdout); 
+			
+
         }
     }
 
