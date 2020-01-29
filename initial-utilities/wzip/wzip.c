239441ff342fdb7a850bbb2c05907e86a5cbbe88
Mihhail Golikov - tug42787 <misha1998@gmail.com>
2020-01-29 03:14:38 -0500

    Add files via upload
    Implemented multiple file handling, no file handling, revamped the entire wzip algorithm, now fwrite(ing) each int (num of char reps) and each corresponding char. Fixed minor bugs.
    
    To fix: repcount is either not entered by wzip properly, or not interpreted by wunzip properly. First rep of char is formatted/zipped/unzipped correctly, following reps of chars are not.

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	// see if there are files to read, if not, exit unsuccessfully
+	if (argc == 1) {
+		printf("wzip: file1 [file2 ...]\n");
+	exit(1);
+	}
 
-    char c[4096]; 
-	int	c2[4096];
-    FILE *fp = fopen(argv[1], "rb");
-
-	int rep;
-
-
-
-    while (!feof(fp)) {
-        size_t bytes = fread(c, 1, sizeof(c), fp);
-        if (bytes) {
-			fwrite(c, 1, bytes, stdout); 
-			printf("\n");
+	FILE * fp;
+	int repcount;
+	char prev_char;
+	char ch;
+	
+	
+	//iterate through all file names
+	for (int i = 1; i < argc; i++) {
+		//initialize repcount variable to zero
+		repcount = 0;
+		//open the file
+		fp = fopen(argv[i], "rb");
+		// when you begin, the first char you encounter is set to prev_char
+		prev_char = fgetc(fp);
+		
+		repcount++; // increment, traversing through each element in txt
+		do { // do {...} while...
 			
-			if(fread(c,1,sizeof(c), fp) == fread(c+1,1,sizeof(c+1), fp)) { // while the current character is the same as the next character
-				printf("if statement");
-				rep++;
-				if (fread(c,4,sizeof(c), fp) != fread(c+1,4,sizeof(c+1), fp)) { // used when the next char is different than current char, to break out of loop
-					// fwrite the int and the char
-					printf("rep number %d of char", &rep);
-					rep = 0;
-					break; // break out of it
-				}
-				break;
+			ch = fgetc(fp); // set ch and the next char
+			
+			if (ch == prev_char) { // check if the current char is the same as the previous char, if so, increment repcount
+				repcount++;
 			}
-			else
-				fwrite(c, 1, bytes, stdout); 
-			
-
-        }
-    }
-
-    //close file
-    fclose(fp);
-
-    return 0;
-
+			else {
+				// if the current and previous char aren't the same,
+				// the repetitions have fpished, so fwrite the number of repetitions
+				// in binary, and the char that was repeated right after it
+				fwrite(&repcount, 4, 1, stdout); // output int in binary
+				fwrite(&prev_char, 1, 1, stdout); // output repeated char
+				repcount = 0; // reset repcount for next char repetition, if there is one
+			}
+			prev_char = ch; // set previous char to current char
+		} while (!feof(fp)); // while not end of file
+	}
+	fclose(fp);
 }
