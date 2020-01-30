0aa62fdbb8c517dc7f7c4ac9a3c4ea882d22a5d2
Mihhail Golikov - tug42787 <misha1998@gmail.com>
2020-01-29 22:49:29 -0500

    Add files via upload
    Rearranged loops and statements in order to parse/interpret the compressed file correctly, making sure that fread doesn't affect inapplicable items, mostly while outputting the characters. Added no file handling, and fixed minor bugs, as well as documented out my code.

--- initial-utilities/wunzip/wunzip.c
+++ initial-utilities/wunzip/wunzip.c
@@ -3,38 +3,47 @@
 
 int main(int argc, char *argv[])
 {
-	// see if there are files to read, if not, exit unsuccessfully
-	if (argc == 1) {
-		printf("wunzip: file1 [file2 ...]");
-	exit(1);
+	FILE * fp; // file pointer used for reading in the .z file
+	char ch; // the character to be repeated
+	int repcount = 0; // number of repetitions for each character
+	int i = 0; // file incrementing variable
+	int j = 0; // char incrementing variable (to traverse and write out all of the chars)
+
+	if (argc < 2) {
+		printf("wunzip: file1 [file2 ...]"); // file not found 
+		exit(1); // exit unsuccessfully
 	}
-
-	FILE * fp;
-	char ch;
-	int j;
-	int repcount;
-	
-	//iterate through each file
-	for (int i = 1; i < argc; i++)
-	{
-		fp = fopen(argv[i], "rb");
-
-		do {
-			//read the first 4 bytes which is equal to
-			//count of character
-			fread(&repcount, 4, 1, fp);
-			//read the 5th byte, which is the character
-			fread(&ch, 1, 1, fp);
-			//print the character count times
+	//loop through all the input files
+	for (i = 1; i < argc; i++) {
+		repcount = 0;
+		//check if the file is present, else continue for next file
+		if (!(fp = fopen(argv[i], "r"))) {
+			printf("wunzip: searchterm [file...]\n"); // cannot open this file, lets try the next
+			continue; // continues when condition is false
+		}
+		//read the opened file
+		while (1) { // while(1) lets the loop run infinitely
+			if (fread(&repcount, sizeof(repcount), 1, fp) <= 0) { 
+			// if repcount is less than or equal to 0
+			// you are, theoretically, printing a char ch 0 times, which is useless,
+			// so break the loop (otherwise it runs infinitely)
+				break;
+			}
+			if ((ch = fgetc(fp)) == EOF) {
+			// if the current character in the file is the end of file, then
+			// break the loop (otherwise it runs infinitely)
+				break;
+			}
 			for (j = 0; j < repcount; j++) {
+			// for the determined amount of repetitions, print the char that was
+			// repeated, repcount times, to stdout
+				//printf("%c", ch);      //used for testing
 				putc(ch,stdout);
 			}
-			// original: aaaaaaaaaabbbb
-			// unzipped: aaaaaaaaaabbbbbb
-			// unzipped: aaaaaaaaaabbbb
-			// unzipped: aaaaaaaaaabbbb
-			// why does the second instance of a char add two extra chars?
-		} while (!feof(fp));
+			// reset the repetition count after done writing each respective char
+			repcount = 0;
+		}
+		// close the file to avoid possible problems
+		fclose(fp);
 	}
-	fclose(fp);
 }
